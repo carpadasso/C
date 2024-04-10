5e08c22083c53d75a42115716878aa81a65648b4
Leonardo Carpwiski <lionheartcarpwiski@gmail.com>
2024-04-09 21:40:07 -0300

    Update io.h
    Alteração para tipoVariavel.nome ter 1024 caracteres.

--- a2/io.h
+++ a2/io.h
@@ -2,6 +2,8 @@
 #define __IO__
 
 #define _CTR_SECURE_NO_WARNINGS
+#define MAX_BUF 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,7 +14,7 @@
 enum tipoVariavel { NUMERIC, STRING };
 
 typedef struct {
-   char* nome;
+   char nome[MAX_BUF];
    enum tipoVariavel tipo;
 } variavel;
 
